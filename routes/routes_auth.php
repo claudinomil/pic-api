0af21e14a9888d4dc3916c546a4c9afc52489615
claudinomil <claudinomoraes@yahoo.com.br>
2023-07-06 15:41:57 -0300

    Produção 05/07/2023 2

--- routes/routes_auth.php
+++ routes/routes_auth.php
@@ -2,6 +2,6 @@
 //password_resets
 use App\Http\Controllers\PasswordResetsController;
 
-Route::post('password_resets/{token}', [PasswordResetsController::class, 'store']);
-Route::post('password_resets_confirm', [PasswordResetsController::class, 'confirm']);
-Route::post('password_resets_update_delete', [PasswordResetsController::class, 'update_delete']);
+Route::post('password_resets/{token}/store', [PasswordResetsController::class, 'store']);
+Route::post('password_resets_confirm/store', [PasswordResetsController::class, 'confirm']);
+Route::post('password_resets_update_delete/store', [PasswordResetsController::class, 'update_delete']);
